1694c1effb6475ed24a5829ae989fb53b59e8bbf
lukianets567 <71081602+lukianets567@users.noreply.github.com>
2023-03-03 19:17:13 +0200

    ///

--- app/src/main/java/com/example/testv1/TestShape.kt
+++ app/src/main/java/com/example/testv1/TestShape.kt
@@ -1,6 +1,7 @@
 package com.example.testv1
 
 import android.content.Context
+import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
@@ -14,17 +15,23 @@ class TestShape(context: Context, attributeSet: AttributeSet) : LinearLayout(con
 
         val attrs = context.obtainStyledAttributes(attributeSet, R.styleable.TestShape)
 
-        binding.
+        val state = attrs.getTextArray(R.styleable.TestShape_state, )
 
-        binding.textView.text = attrs.getString(R.styleable.CustomCard_text)
-        binding.checkBox.isChecked = attrs.getBoolean(R.styleable.CustomCard_checked, false)
+
+        //binding.cardView.state =
+
+            // binding.cardView.state = attrs.getString(R.styleable.TestShape_state)
+
+        //binding.textView.text = attrs.getString(R.styleable.CustomCard_text)
+        //binding.checkBox.isChecked = attrs.getBoolean(R.styleable.CustomCard_checked, false)
+
 
         attrs.recycle()
 
-        binding.textView.setOnClickListener {
-            binding.textView.text = check()
+        //binding.textView.setOnClickListener {
+            //binding.textView.text = check()
         }
+
     }
 
-    private fun check(): String = binding.checkBox.isChecked.toString()
-}
+    //private fun check(): String = binding.checkBox.isChecked.toString()

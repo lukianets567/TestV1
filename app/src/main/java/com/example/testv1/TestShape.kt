f0bd81aceeebdf88c3002929f1f7718248a4adeb
lukianets567 <71081602+lukianets567@users.noreply.github.com>
2023-03-10 10:38:12 +0200

    ...

--- app/src/main/java/com/example/testv1/TestShape.kt
+++ app/src/main/java/com/example/testv1/TestShape.kt
@@ -7,7 +7,8 @@ import android.view.LayoutInflater
 import android.widget.LinearLayout
 import com.example.testv1.databinding.TestShapeBinding
 
-class TestShape(context: Context, attributeSet: AttributeSet) : LinearLayout(context, attributeSet) {
+class TestShape(context: Context, attributeSet: AttributeSet):LinearLayout(context, attributeSet){
+
     private lateinit var binding: TestShapeBinding
 
     init {
@@ -15,23 +16,45 @@ class TestShape(context: Context, attributeSet: AttributeSet) : LinearLayout(con
 
         val attrs = context.obtainStyledAttributes(attributeSet, R.styleable.TestShape)
 
-        val state = attrs.getTextArray(R.styleable.TestShape_state, )
+        //val state = attrs.getTextArray(R.styleable.TestShape_state,)
+
+        //val state = binding.cardView.state.
 
 
-        //binding.cardView.state =
+        //updateState()
+
+        fun updateState(state: String) {
+            if (state == "stop") {
+                binding.stop.setImageResource(R.drawable.test)
+                binding.ready.setImageResource(R.drawable.ready_b)
+                binding.go.setImageResource(R.drawable.go_b)
+            }
+            if (state == "ready") {
+                binding.stop.setImageResource(R.drawable.stop_b)
+                binding.ready.setImageResource(R.drawable.test1)
+                binding.go.setImageResource(R.drawable.go_b)
+            }
+            if (state == "go") {
+                binding.stop.setImageResource(R.drawable.stop_b)
+                binding.ready.setImageResource(R.drawable.ready_b)
+                binding.go.setImageResource(R.drawable.test2)
+            }
+
+
+
 
             // binding.cardView.state = attrs.getString(R.styleable.TestShape_state)
 
-        //binding.textView.text = attrs.getString(R.styleable.CustomCard_text)
-        //binding.checkBox.isChecked = attrs.getBoolean(R.styleable.CustomCard_checked, false)
 
 
-        attrs.recycle()
+            attrs.recycle()
 
-        //binding.textView.setOnClickListener {
-            //binding.textView.text = check()
         }
 
+
     }
+
+}
+
 
     //private fun check(): String = binding.checkBox.isChecked.toString()

4fbf9d421af53219597c497cd99b46347c53d137
tm00nlight <95237741+tm00nlight@users.noreply.github.com>
2023-04-09 17:57:59 +0300

    solution

---
+++ app/src/main/java/com/example/testv1/Example.kt
@@ -0,0 +1,7 @@
+package com.example.testv1
+
+enum class Example (val index: Int) {
+    NEW(0),
+    DOWNLOADING(1),
+    FINISHED(2)
+}
--- app/src/main/java/com/example/testv1/TestShape.kt
+++ app/src/main/java/com/example/testv1/TestShape.kt
@@ -2,6 +2,7 @@ package com.example.testv1
 
 import android.content.Context
 import android.content.res.TypedArray
+import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
@@ -10,50 +11,42 @@ import com.example.testv1.databinding.TestShapeBinding
 class TestShape(context: Context, attributeSet: AttributeSet):LinearLayout(context, attributeSet){
 
     private lateinit var binding: TestShapeBinding
+    private var state: Int = 0
 
     init {
         binding = TestShapeBinding.inflate(LayoutInflater.from(context), this, true)
 
         val attrs = context.obtainStyledAttributes(attributeSet, R.styleable.TestShape)
+        state = attrs.getInt(R.styleable.TestShape_state, 0)
+        updateState()
 
-        //val state = attrs.getTextArray(R.styleable.TestShape_state,)
+        val color = attrs.getColor(R.styleable.TestShape_color, Color.GRAY)
+        binding.cardView.setBackgroundColor(color)
 
-        //val state = binding.cardView.state.
+        attrs.recycle()
 
+    }
 
-        //updateState()
-
-        fun updateState(state: String) {
-            if (state == "stop") {
+    private fun updateState() {
+        when (state) {
+            0 -> {
                 binding.stop.setImageResource(R.drawable.test)
                 binding.ready.setImageResource(R.drawable.ready_b)
                 binding.go.setImageResource(R.drawable.go_b)
             }
-            if (state == "ready") {
+            1 -> {
                 binding.stop.setImageResource(R.drawable.stop_b)
                 binding.ready.setImageResource(R.drawable.test1)
                 binding.go.setImageResource(R.drawable.go_b)
             }
-            if (state == "go") {
+            2 -> {
                 binding.stop.setImageResource(R.drawable.stop_b)
                 binding.ready.setImageResource(R.drawable.ready_b)
                 binding.go.setImageResource(R.drawable.test2)
             }
-
-
-
-
-            // binding.cardView.state = attrs.getString(R.styleable.TestShape_state)
-
-
-
-            attrs.recycle()
-
         }
 
-
     }
-
 }
 
 
